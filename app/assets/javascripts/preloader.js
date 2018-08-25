bcefdaab4183f7ec9da4effafa47438a78461566
Reymundo Tenorio <reymundotenorio@gmail.com>
2018-08-25 11:51:48 -0600

    Modificando variables de colores y agregando Script para el preloader

---
+++ app/assets/javascripts/preloader.js
@@ -0,0 +1,9 @@
+// Turbolinks loading
+$(document).on("turbolinks:click", function() {
+  $(".preloader").show();
+});
+
+// Turbolinks loaded
+$(document).on("turbolinks:load", function() {
+  $(".preloader").hide();
+});
