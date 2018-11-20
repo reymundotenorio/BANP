349439a1fb4b998b634d6849610ca7d01f775244
Reymundo Tenorio <reymundotenorio@gmail.com>
2018-11-19 21:57:41 -0600

    Agregando cuerpo del landing

--- app/assets/javascripts/tools.js
+++ app/assets/javascripts/tools.js
@@ -1,4 +1,6 @@
-$(document).on("turbolinks:load", function() {
+// $(document).on("turbolinks:load", function() {
+$( document ).ready(function() {
+  
   $('.animated').each(function() {
     var item = $(this)
     var delay = $(this).data("delay")
