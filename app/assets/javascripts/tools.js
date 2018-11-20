f5bce456cc59a82890c3918caa74ff706685ae8d
Reymundo Tenorio <reymundotenorio@gmail.com>
2018-11-20 17:58:17 +0000

    Agregando traduccion a sitio cliente

--- app/assets/javascripts/tools.js
+++ app/assets/javascripts/tools.js
@@ -1,6 +1,5 @@
-// $(document).on("turbolinks:load", function() {
-$( document ).ready(function() {
-  
+$(document).on("turbolinks:load", function() {
+
   $('.animated').each(function() {
     var item = $(this)
     var delay = $(this).data("delay")
@@ -15,6 +14,7 @@ $( document ).ready(function() {
       }, delay);
     }
   });
+  
 });
 
 
