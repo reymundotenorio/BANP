b89058920ad1de12979ff79a07bc94cc760c26d1
Reymundo Tenorio <reymundotenorio@gmail.com>
2018-12-07 19:10:35 +0000

    Generando controlador y vista para auth notifications

--- app/controllers/authentication/notifications_controller.rb
+++ app/controllers/authentication/notifications_controller.rb
@@ -1,4 +1,11 @@
 class Authentication::NotificationsController < ApplicationController
+  # Authentication layout
+  layout "admin/authentication"
+  # End Authentication layout
+
+  # /authentication/notifications
   def index
+    @source = params[:source]
+    @source = "not-found" unless @source
   end
 end
