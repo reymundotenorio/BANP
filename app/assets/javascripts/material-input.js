ed1ddca466eb654c88c6012a68ac5b94607b66a4
Reymundo Tenorio <reymundotenorio@gmail.com>
2019-02-03 01:07:44 -0600

    Reparando input de search y removiendo controles de number inputs en Firefox

--- app/assets/javascripts/material-input.js
+++ app/assets/javascripts/material-input.js
@@ -31,6 +31,18 @@ $(document).ready(function(){
     }
   });
 
+  // Inputs search
+  $("input.search-input").on("keyup change", function(){
+    // If empty
+    if($(this).val() == ""){
+      $(this).removeClass("with-value"); // Remove class
+    }
+
+    else{
+      $(this).addClass("with-value"); // Add class
+    }
+  });
+
   // Inputs
   $("input.form-control").each(function(){
     // If empty
@@ -55,8 +67,8 @@ $(document).ready(function(){
     }
   });
 
-  // Inputs change
-  $("input.form-control").change(function(){
+  // Inputs search
+  $("input.search-input").each(function(){
     // If empty
     if($(this).val() == ""){
       $(this).removeClass("with-value"); // Remove class
