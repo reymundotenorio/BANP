cc99321360a98ad21be33d039b859a1e6de6a0e5
Reymundo Tenorio <reymundotenorio@gmail.com>
2019-02-22 00:52:21 -0600

    Agregando detalle de compra, convertir a inputs los campos

--- app/assets/javascripts/select-product.js
+++ app/assets/javascripts/select-product.js
@@ -1,20 +1,2 @@
 $(document).ready(function(){
-
-  // Select Provider
-  $(".select-product").each(function(){
-
-    $(this).click(function(){
-      product_id = $(this).attr("data-id");
-      product_name = $(this).attr("data-name");
-
-      $("#purchase_product").val(product_name).change();
-      $("#purchase_product_id").val(product_id);
-
-      $("#search-input").val("");
-      $("#getProviders").modal("hide");
-    });
-
-  });
-
-
 });
